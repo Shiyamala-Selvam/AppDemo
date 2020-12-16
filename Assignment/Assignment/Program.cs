dc55810b600b853ea966c02145d8696dbdf43062
unknown <Shiyamala.Selvam@lntinfotech.com>
2020-12-16 17:05:09 +0530

    update

--- Assignment/Assignment/Program.cs
+++ Assignment/Assignment/Program.cs
@@ -10,6 +10,26 @@ namespace Assignment
     {
         static void Main(string[] args)
         {
+            int m, n;
+            bool flag= false;
+            Console.WriteLine("Enter range");
+            m = Convert.ToInt32(Console.ReadLine());
+            n = Convert.ToInt32(Console.ReadLine());
+            for(int i=m;i<=n;i++)
+            {
+                for(int j=2;j<i;j++)
+                {
+                    if(i%j==0)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag)
+                    Console.WriteLine(i);
+                else
+                    flag = false;
+            }
         }
     }
 }
